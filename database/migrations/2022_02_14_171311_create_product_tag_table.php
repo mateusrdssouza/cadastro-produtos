d5a93256dab6032943f42b7c564a8361af186647
mateusrdssouza <mateusrdssouza@gmail.com>
2022-02-14 15:09:32 -0300

    Criação das migrations

---
+++ database/migrations/2022_02_14_171311_create_product_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTagTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('product_tag', function (Blueprint $table) {
+			$table->integer('product_id');
+			$table->integer('tag_id');
+			$table->primary(['product_id', 'tag_id']);
+			$table->foreign('product_id', 'product_id')->references('id')->on('product');
+			$table->foreign('tag_id', 'tag_id')->references('id')->on('tag');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('product_tag');
+	}
+}
